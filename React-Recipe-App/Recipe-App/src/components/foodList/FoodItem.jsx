bc910f517c4cf5525cfef993fb6405ba0d7ff63a
vinald <okiror1vinald@gmail.com>
2025-06-29 13:43:32 +0300

    food List

---
+++ React-Recipe-App/Recipe-App/src/components/foodList/FoodItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./FoodItem.module.css";
+
+function FoodItem({ food }) {
+    return (
+        <div className={styles.itemContainer}>
+            <img className={styles.itemImage} src={food.image} alt={food.title} />
+            <div className={styles.itemContent}>
+                <p className={styles.itemTitle}>{food.title}</p>
+            </div>
+            <div className={styles.buttonContainer}>
+                <button className={styles.itemButton}>View Recipe</button>
+            </div>
+        </div>
+    );
+}
+
+export default FoodItem;
--- React-Recipe-App/Recipe-App/src/components/searchBar/Search.jsx
+++
@@ -1,9 +0,0 @@
-function Search() {
-    return (
-        <div>
-            <input type="text" placeholder="Search for recipes..." className="search-input" />
-        </div>
-    );
-}
-
-export default Search;
