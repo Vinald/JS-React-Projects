712ea98214dc844028ee7bbb1334c664c1c279db
vinald <okiror1vinald@gmail.com>
2025-06-29 11:26:17 +0300

    project folder

---
+++ React-Recipe-App/Recipe-App/src/App.jsx
@@ -0,0 +1,12 @@
+import Search from "./components/searchBar/Search";
+
+function App() {
+    return (
+        <>
+            <Search />
+            trsdfd
+        </>
+    );
+}
+
+export default App;
---
+++ React-Recipe-App/Recipe-App/src/components/searchBar/Search.jsx
@@ -0,0 +1,9 @@
+function Search() {
+    return (
+        <div>
+            <input type="text" placeholder="Search for recipes..." className="search-input" />
+        </div>
+    );
+}
+
+export default Search;
